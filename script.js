8f49028d68407c7e33b9ec946ee08841b61e4d47
Xs3roN <xs3ron@gmail.com>
2020-08-17 18:38:23 +0200

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+redirect1 = () => {
+   location.href = "https://xs3ron.github.io/hangman/"
+ }
+redirect2 = () => {
+   location.href = ""
+ }
+redirect3 = () => {
+   location.href = ""
+ }
+redirect4 = () => {
+   location.href = ""
+ }
